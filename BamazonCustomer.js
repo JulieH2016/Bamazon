14a93275e96d68fc34f630720d5e5e59979dbd8e
Julie Hoffman <jhoffman2013@gmail.com>
2017-01-22 20:58:36 -0500

    Add prompt to ask customer how many units they want to buy

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -38,8 +38,30 @@ var correct = false;
 for (var i = 0; i <res.length; i++) {
 	if(res[i].ItemID==answer.choice){
 		correct=true;
-		var product=answer.choice;
-		var id=i;
+var ItemID=answer.choice;
+var id=i;
+	inquirer.prompt({
+		type:"input",
+		name:"quant",
+		message:"How many units of this product would you like to purchase?",
+		validate: function(value){
+			if(isNaN(value)==false){
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}).then(function(answer){
+		if((res[id].StockQuantity=answer.quant)>0){
+			connection.query("UPDATE products SET StockQuantity='"+(res[id].StockQuantity-answer.quant)+"' WHERE ItemID='"+product+"'", function(err,res2){
+			console.log("Product Purchased!");
+			makeTable();
+			})
+		} else {
+			console.log("Sorry, insufficient quantity in stock at this time.");
+			promptCustomer(res);
+					}
+				})
 			} 
 		}
 	})
